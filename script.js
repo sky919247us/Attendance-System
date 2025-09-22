f26347a902c55d9df8be7ae8e7d1e207cdca30d0
wenhuistage-ops <0rigin.d.1865@gmail.com>
2025-09-22 18:29:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -241,8 +241,10 @@ function renderCalendar(date) {
            // 直接用快取資料過濾當天紀錄
            console.log(records);
            console.log(dateKey);
-           const todayRecords = records.filter(r => r.date === dateKey);
-           console.log(r.date);
+           const todayRecords = records.filter(r =>{
+               console.log(r.date);
+               return r.date === dateKey});
+           
            console.log(todayRecords.length);
            if (todayRecords.length > 0) {
                // 依照 reason 給 class，取第一筆 reason 或自定義邏輯
