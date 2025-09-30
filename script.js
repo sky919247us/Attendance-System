9895133c3f28ec1f2249a9b0507d6ce60dd22636
wenhuistage-ops <0rigin.d.1865@gmail.com>
2025-09-30 10:26:09 +0800

    renderTranslations(calendarGrid);

--- script.js
+++ script.js
@@ -241,7 +241,7 @@ async function renderCalendar(date) {
         // 如果沒有，才發送 API 請求
         // 清空日曆，顯示載入狀態，並確保置中
         calendarGrid.innerHTML = '<div data-i18n="LOADING" class="col-span-full text-center text-gray-500 py-4">正在載入...</div>';
-        
+        renderTranslations(calendarGrid);
         try {
             const res = await callApifetch(`getAttendanceDetails&month=${monthkey}&userId=${userId}`);
             if (res.ok) {
