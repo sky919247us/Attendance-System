4e4b91cd7facd46ecd1c6c25b5f2bc20be856cd5
sky919247us <sky919247us@gmail.com>
2025-10-22 21:17:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbzvZPwRXNBfbsi9YICkDCu3lfTRjofMth_OwViQzGVGkoba-2yunqlHQluEp3QF_qzh/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwVcmWGAGHMl2iuxBcLFs2GqEFejQeDY6WiZJ7jV6rV4PIdCxc60JNr4w_e1V9Cx8kb0g/exec",
   // 新增回呼網址
   redirectUrl: "https://0rigind1865-bit.github.io/Attendance-System/"
   // 你也可以在這裡加入其他設定，例如：
