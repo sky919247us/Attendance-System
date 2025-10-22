0acdc3b7e076c4c0df047e6bdccdedb23728e262
sky919247us <sky919247us@gmail.com>
2025-10-22 22:39:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const API_CONFIG = {
   // 正式環境的 API URL
   apiUrl: "https://script.google.com/macros/s/AKfycbwVcmWGAGHMl2iuxBcLFs2GqEFejQeDY6WiZJ7jV6rV4PIdCxc60JNr4w_e1V9Cx8kb0g/exec",
   // 新增回呼網址
-  redirectUrl: "https://script.google.com/macros/s/AKfycbwVcmWGAGHMl2iuxBcLFs2GqEFejQeDY6WiZJ7jV6rV4PIdCxc60JNr4w_e1V9Cx8kb0g/exec"
+  redirectUrl: "https://sky919247us.github.io/Attendance-System/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v1.0'
